be120f3be52e8e4c21d418e267b8d2650e8b3038
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2023-07-20 23:28:25 +0530

    Time: 20 ms (29.55%) | Memory: 18.9 MB (8.30%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        vector<int> ans;
+        int n = arr.size();
+        vector<bool> left(n, true);   
+        vector<int> brr;
+        for(int i = 1; i<n; i++) 
+            if(arr[i-1] > 0 and arr[i] < 0) 
+                brr.push_back(i-1); // Check for Collision.
+        
+        if(brr.size() == 0) return arr; // Base Case
+        
+        int r;
+        for(int l: brr){
+            r = l + 1;
+            while(l>=0 and r<n and arr[l] > 0 and arr[r] < 0)
+                if(abs(arr[l]) == abs(arr[r])) left[l--] = left[r++] = false;
+                else if(abs(arr[l]) < abs(arr[r])) left[l--] = false;
+                else left[r++] = false;
+        }
+        
+        for(int i = 0; i<n; i++) if(left[i]) ans.push_back(arr[i]);
+        
+        return asteroidCollision(ans); // rec
+    }
+};
