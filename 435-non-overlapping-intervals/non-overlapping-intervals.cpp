670e15d5cbe262d9b2f0955881ebb767e1cfd64a
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2023-08-17 01:22:44 +0530

    Time: 361 ms (93.73%) | Memory: 89.8 MB (26.92%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,13 +1,13 @@
-bool comparator(const std::vector<int>& a, const std::vector<int>& b) {
+bool cmp(vector<int>& a, vector<int>& b) {
     return a[1] < b[1];  // Compare based on the second column (index 1)
 }
-class Solution { //daily challenge submit without solve
+class Solution { //self 100
 public:
 
     int eraseOverlapIntervals(vector<vector<int>>& v) {
         int n=v.size();
         if(n<=1) return 0;
-        sort(v.begin(),v.end(),comparator);
+        sort(v.begin(),v.end(),cmp);
         int ans=0;
         pair<int,int> prev={v[0][0],v[0][1]};
         for(int i=1;i<n;i++){
