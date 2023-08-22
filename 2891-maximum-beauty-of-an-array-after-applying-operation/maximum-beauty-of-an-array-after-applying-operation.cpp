39f27a1abeef25a0a99c65b1d6158cb1bb3322fe
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2023-08-22 15:09:31 +0530

    Time: 189 ms (95.48%) | Memory: 100.5 MB (82.02%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int maximumBeauty(vector<int>& nums, int k) {
-        int cnt=1,n=nums.size();
+    int maximumBeauty(vector<int>& nums, int k) {//The beauty of the array is the len
+        int n=nums.size();
         if(n==1)return 1;
         sort(nums.begin(), nums.end());
-        int index=0,ans=0;
         if(n==1) return nums[0];
+        int index=0,ans=0,cnt=1;
         for(int i=1;i<n;i++){
             if((nums[i]-k)<=(nums[index]+k)){
                 cnt++;
                 ans=max(ans,cnt);
-                // cout<<" if "<<nums[i]<<endl;
             }
             else{
                 cnt--;
