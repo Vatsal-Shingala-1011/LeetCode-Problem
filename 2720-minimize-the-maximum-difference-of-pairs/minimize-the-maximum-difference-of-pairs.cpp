374a599cf50cac8445429c9ce67186cda08de7c1
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2023-08-22 15:52:44 +0530

    Time: 120 ms (83.87%) | Memory: 81.2 MB (52.46%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -4,14 +4,15 @@ public:
         sort(A.begin(), A.end());
         int n = A.size(), left = 0, right = A[n - 1] - A[0];
         while (left < right) {
-            int mid = (left + right) / 2, k = 0;
+            int mid = (left + right) / 2;
+            int k = 0;
             for (int i = 1; i < n && k < p; ++i) {
                 if (A[i] - A[i - 1] <= mid) {
                     k++;
                     i++;
                 }
             }
-            if (k >= p)
+            if (k == p)
                 right = mid;
             else
                 left = mid + 1;
