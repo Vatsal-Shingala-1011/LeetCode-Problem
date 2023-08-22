5406a6170ac752980de872dc14ee410ec3d38d8c
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2023-08-22 16:03:02 +0530

    Time: 5 ms (60.53%) | Memory: 12.8 MB (28.80%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution { //O(1) extra space
+//// another approch is make vector of inorder of tree and by traversing it make ll
+public:  
+    void flatten(TreeNode* root) {
+        TreeNode* curr=root;
+        while(curr!=NULL)
+        {
+            if(curr->left){
+                TreeNode* predecessor=curr->left;
+                while(predecessor->right){//not null
+                    predecessor=predecessor->right;
+                }
+                predecessor->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;         
+        }
+    }
+};
