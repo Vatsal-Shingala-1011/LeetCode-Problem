53d1234387d65726421f6749a90361c7cebf144b
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2024-07-25 23:21:41 +0530

    Time: 30 ms (81.61%) | Memory: 36.1 MB (46.01%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) { //todo
-        int rows = rowSum.size();
-        int cols = colSum.size();
-        int cur_row = 0, cur_col = 0;
-        vector<vector<int>> res(rows, vector<int>(cols, 0));
-
-        while (cur_row < rows || cur_col < cols) {
-            if (cur_row >= rows) {
-                res[rows - 1][cur_col] = colSum[cur_col];
-                cur_col++;
-                continue;
-            } else if (cur_col >= cols) {
-                res[cur_row][cols - 1] = rowSum[cur_row];
-                cur_row++;
-                continue;
-            }
-
-            int value_to_put = min(rowSum[cur_row], colSum[cur_col]);
-            rowSum[cur_row] -= value_to_put;
-            colSum[cur_col] -= value_to_put;
-            res[cur_row][cur_col] = value_to_put;
-
-            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
-            if (rowSum[cur_row] == 0) {
-                cur_row++;
-            }
-            if (colSum[cur_col] == 0) {
-                cur_col++;
-            }
+        int n = rowSum.size();
+        int m = colSum.size();
+        int i = 0, j = 0;
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        while(i<n && j<m){
+            int x=min(rowSum[i],colSum[j]);
+            ans[i][j]=x;
+            rowSum[i]-=x;
+            colSum[j]-=x;
+            if(rowSum[i]==0)i++;
+            if(colSum[j]==0)j++;
         }
-        return res;
+        return ans;
     }
 };
