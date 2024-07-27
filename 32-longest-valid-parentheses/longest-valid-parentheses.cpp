799d2f8af3686b5e0a5527481c233abe2a5a1dc0
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2024-07-27 18:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.15%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        int o = 0, c = 0, ans = 0, temp = 0;
+        // Left to right pass
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                o++;
+            } else {
+                c++;
+            }
+            if (o == c) {
+                ans = max(ans, 2 * c);
+            } else if (c > o) {
+                o = c = 0;
+            }
+        }
+        o = c = 0;
+        // Right to left pass
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] == '(') {
+                o++;
+            } else {
+                c++;
+            }
+            if (o == c) {
+                ans = max(ans, 2 * o);
+            } else if (o > c) {
+                o = c = 0;
+            }
+        }
+
+        return ans;
+    }
+};
