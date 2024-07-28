faa3446083d881ad4a766df26de3538508a4a542
Vatsal Shingala <127982807+Vatsal-Shingala-1011@users.noreply.github.com>
2024-07-28 23:00:10 +0530

    Time: 2 ms (50.60%) | Memory: 7.4 MB (11.24%) - LeetSync

---
+++ 888-mirror-reflection/mirror-reflection.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    //given: the test cases are guaranteed so that the ray will meet a receptor eventually.
+    public:
+    int mirrorReflection(int p, int q) {
+        while(p%2==0 && q%2==0){
+            p=p/2;
+            q=q/2;
+        }
+        if(p%2==0) return 2;
+        if(q%2==0) return 0;
+        return 1;
+    }
+};
